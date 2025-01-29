b4c0a127381f47541bb3b24683509f3054274e7b
Babar Javaid <babar.javaid@simubladetechnology.com>
2025-01-29 23:09:04 +0500

    Corrected Page Title

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -3,8 +3,8 @@ import Home from "@/components/Home";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "Bin Iqbal Group" },
+    { name: "description", content: "Bin Iqbal Group" },
   ];
 };
 
